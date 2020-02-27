52d9cf0d538fcbfcf04005a243e2ff0556dd85b3
Jack Zhang <zzj1353832196@gmail.com>
2020-02-27 02:57:40 -0500

    Add BottomNavigationView with fragment features.

--- app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.coen_elec_390_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListerner);
     }
+
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListerner = new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.profile:
+                            selectedFragment = new ProfileActivity();
+                            break;
+                        case R.id.states:
+                            selectedFragment = new StatisticActivity();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+                    return true;
+                }
+     };
 }
---
+++ app/src/main/java/com/example/coen_elec_390_project/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.coen_elec_390_project;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ProfileActivity extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_profile, container, false);
+    }
+}
