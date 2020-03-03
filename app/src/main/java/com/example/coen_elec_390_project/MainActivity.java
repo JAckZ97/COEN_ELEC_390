239515164ad4ed2b6b0795646f094f08c95d628e
Ling <mlz961116@gmail.com>
2020-03-03 15:36:05 -0500

    Add bluetooth module on android studio

--- app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
@@ -4,20 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.ParcelUuid;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
     TextView bpm;
+    private BluetoothAdapter bluetoothAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private boolean scanning;
+
+    private Handler mHandler;
+    List<BluetoothDevice> listBluetoothDevice;
+    private static final long SCAN_PERIOD = 10000;
+    private BluetoothGattCallback gattCallback;
+    private BluetoothGatt bluetoothGatt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +60,35 @@ public class MainActivity extends AppCompatActivity {
         bpm.setText("180 bpm");
         bpm.setBackgroundResource(R.drawable.ic_bpm);
         bpm.setTextColor(getResources().getColor(R.color.colorPrimary));
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        getBluetoothAdapterAndLeScanner();
+        // Checks if Bluetooth is supported on the device.
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this,
+                    "bluetoothManager.getAdapter()==null",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        scanLeDevice(true);
+        if(listBluetoothDevice.size()==1){
+            bluetoothGatt = listBluetoothDevice.get(0).connectGatt(this, false, gattCallback);
+        }
+
+    }
+
+
+    private void getBluetoothAdapterAndLeScanner(){
+        // Get BluetoothAdapter and BluetoothLeScanner.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        mBluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        scanning = false;
     }
 
     private void setUpBottomNavigationView() {
@@ -59,4 +118,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            listBluetoothDevice.clear();
+
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mBluetoothLeScanner.stopScan(scanCallback);
+
+                    Toast.makeText(MainActivity.this,
+                            "Scan timeout",
+                            Toast.LENGTH_LONG).show();
+
+                    scanning = false;
+                }
+            }, SCAN_PERIOD);
+
+            //mBluetoothLeScanner.startScan(scanCallback);
+
+            //scan specified devices only with ScanFilter
+            ScanFilter scanFilter =
+                    new ScanFilter.Builder()
+                            .setServiceUuid(new ParcelUuid(UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")))
+                            .build();
+            List<ScanFilter> scanFilters = new ArrayList<ScanFilter>();
+            scanFilters.add(scanFilter);
+
+            ScanSettings scanSettings =
+                    new ScanSettings.Builder().build();
+
+            mBluetoothLeScanner.startScan(scanFilters, scanSettings, scanCallback);
+            scanning = true;
+        } else {
+            mBluetoothLeScanner.stopScan(scanCallback);
+            scanning = false;
+        }
+    }
+
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+
+            addBluetoothDevice(result.getDevice());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+            for(ScanResult result : results){
+                addBluetoothDevice(result.getDevice());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Toast.makeText(MainActivity.this,
+                    "onScanFailed: " + String.valueOf(errorCode),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        private void addBluetoothDevice(BluetoothDevice device){
+            if(!listBluetoothDevice.contains(device)){
+                listBluetoothDevice.add(device);
+            }
+        }
+    };
 }
