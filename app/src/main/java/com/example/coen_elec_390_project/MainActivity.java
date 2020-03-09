b141f7869c2b90f52e999fbcf1843baacd7937fd
Jack Zhang <zzj1353832196@gmail.com>
2020-03-08 22:16:22 -0400

    guest mode bug fixed

--- app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,12 +12,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
     TextView bpm;
 
     @Override
@@ -32,31 +36,41 @@ public class MainActivity extends AppCompatActivity {
         bpm.setTextColor(getResources().getColor(R.color.colorPrimary));
     }
 
+
     private void setUpBottomNavigationView() {
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-                    case R.id.home:
-                        break;
+            final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                    case R.id.profile:
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-                        break;
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()){
+                        case R.id.home:
+                            break;
 
-                    case R.id.statistics:
-                        startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
-                        break;
+                        case R.id.profile:
+                            if (user == null) {
+                                // User is signed in
+                                startActivity(new Intent(MainActivity.this, StartActivity.class));
+                                break;
+                            } else {
+                                // No user is signed in
+                                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                                break;
+                            }
 
-                    case R.id.logout:
-                        FirebaseAuth.getInstance().signOut();
-                        startActivity(new Intent(MainActivity.this, StartActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                        break;
+                        case R.id.statistics:
+                            startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
+                            break;
+
+                        case R.id.logout:
+                            FirebaseAuth.getInstance().signOut();
+                            startActivity(new Intent(MainActivity.this, StartActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                            break;
+                    }
+
+                    return false;
                 }
-
-                return false;
-            }
-        });
+            });
     }
 }
