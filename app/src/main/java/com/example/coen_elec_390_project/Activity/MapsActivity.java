96dff09c5264e6150733130a2e31076492cd47bb
Ling <mlz961116@gmail.com>
2020-03-22 18:37:19 -0400

    debug map

--- app/src/main/java/com/example/coen_elec_390_project/Activity/MapsActivity.java
+++ app/src/main/java/com/example/coen_elec_390_project/Activity/MapsActivity.java
@@ -104,20 +104,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         polylineOptions.color(Color.CYAN);
         polylineOptions.width(4);
         gpsTrack = mMap.addPolyline(polylineOptions);
-//
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        enableMyLocation();
-        Toast.makeText(this, "Current location:\n" + currentlocation, Toast.LENGTH_LONG).show();
-        Log.e("Tag","<Map> "+"Current location:\n" + currentlocation);
+
+//        Toast.makeText(this, "Current location:\n" + currentlocation, Toast.LENGTH_LONG).show();
+//        Log.e("Tag","<Map> "+"Current location:\n" + currentlocation);
         LatLng latLng= new LatLng(currentlocation.getLatitude(),currentlocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here");
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
